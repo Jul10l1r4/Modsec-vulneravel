a3aca8d1a83ca8339a59a76a5d26616a4b8d367e
jul10l1r4 <jul10l1r4@disroot.org>
2019-10-17 19:39:37 -0300

    :metal: Seting defaults and cookies

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var cookieParser = require('cookie-parser'); 
 var app = express();
+
+app.use(cookieParser());
 
 app.get('/', function(req, res) {
     console.log(`Req ↓ \n Comment: ${req.query.comment}`);
+    res.cookie('fake-session-id', 'josivaldo:senha123:ngm-owna-sapoha');
     res.send(`Comment: ${req.query.comment}`);
 });
 
